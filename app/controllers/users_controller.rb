a7bbfe3aec136a75547bf9795366b7bc27b07d09
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-09-21 17:14:53 +0900

    strong parameterの導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+ def show
+  @user = User.find(params[:id])
+ end
+ 
   def new
+    @user = User.new
+  end
+  #POST users params 
+  def create
+    # (@user + given params).save
+    # User.create(params[:user])
+    # => name, email, pass/confirmation
+    if @user = User.new(user_params) 
+      #Success (valid params)
+    else
+      # failure(not valid params)
+      render 'new' #もう一回newに戻る
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
