4e0d2667151309e28fb7560224cdf4f715571580
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-09-21 18:07:33 +0900

    ログインフラッシュメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
     # => name, email, pass/confirmation
    @user = User.new(user_params) 
    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
       #Success (valid params)
     else
       # failure(not valid params)
